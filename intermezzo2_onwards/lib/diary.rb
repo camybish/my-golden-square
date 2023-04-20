d55cc7b812c540c6e8aa91228f2d6b2109eee673
Cameron <cameronjamesbishop@googlemail.com>
2023-04-20 15:47:07 +0100

    Eagle eyed debug - starting multiclass now

---
+++ intermezzo2_onwards/lib/diary.rb
@@ -0,0 +1,18 @@
+class Diary
+    def initialize(title, artist)
+    end
+
+    def title
+    end
+
+    def contents
+    end
+
+    def count_words
+    end
+
+    def reading_time(wpm)
+    end
+
+    def reading_chunk
+end
