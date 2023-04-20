b286cfaa558c8ec0dce9864c6fad96d1afcf5335
Cameron <cameronjamesbishop@googlemail.com>
2023-04-20 11:28:32 +0100

    VowelRemoved solved(those cheeky counters)

--- intermezzo2_onwards/spec/vowel_remover_spec.rb
+++ intermezzo2_onwards/spec/vowel_remover_spec.rb
@@ -1,13 +1,13 @@
 require 'vowel_remover'
 
 RSpec.describe "remove_vowels method" do
-  xit "removes a vowel from a simple string" do
+  it "removes a vowel from a simple string" do
     remover = VowelRemover.new("ab")
     result_no_vowels = remover.remove_vowels
     expect(result_no_vowels).to eq "b"
   end
 
-  xit "removes vowels from a complex string" do
+  it "removes vowels from a complex string" do
     remover = VowelRemover.new("We will remove the vowels from this sentence.")
     result_no_vowels = remover.remove_vowels
     expect(result_no_vowels).to eq "W wll rmv th vwls frm ths sntnc."
